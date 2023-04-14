c86d8f67a76fb29ea86977141a7f851266052767
AlessioAbbati <alex_117@live.it>
2023-04-14 11:15:29 +0200

    mod click to btn calc

--- js/myscript.js
+++ js/myscript.js
@@ -9,5 +9,28 @@ const btnReset = document.querySelector(".btn-reset");
 const eleOutput = document.getElementById("output");
 
 btnCalc.addEventListener("click", function() {
-    
+    const distance = parseInt(inputDistance.value);
+    const age = parseInt(inputAge.value);
+
+
+    const basePrice = distance * pricePerkm;
+
+
+    let discountPercent = 0;
+    if (age < 18) {
+    discountPercent = discountChildren;
+    } else if (age >= 65) {
+    discountPercent = discountSenior;
+    }
+
+
+    const discount = basePrice * discountPercent / 100;
+    console.log(discount);
+
+    let finalPrice = basePrice - discount;
+
+    finalPrice = parseFloat(finalPrice.toFixed(2));
+    console.log(finalPrice);
+
+    document.getElementById("output").innerHTML = finalPrice.toFixed(2);
 })
