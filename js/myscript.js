ccd771b4db25f6519dd6af48590431ffb88f9d89
AlessioAbbati <alex_117@live.it>
2023-04-14 11:07:21 +0200

    mod  js const

--- js/myscript.js
+++ js/myscript.js
@@ -1,23 +1,3 @@
-const km = parseInt(prompt("inserire i chilometri che si vogliono percorrere"));
-const age = parseInt(prompt("inserire la tua età"));
-
-let biglietto = (0,21 * km);
-let bigliettoScontatoU18 = biglietto - (biglietto * 20 / 100);
-let bigliettoScontatoO65 = biglietto - (biglietto * 40 / 100);
-
-
-if (age < 18) {
-    console.log(bigliettoScontatoU18);
-} 
-   
-if (age > 65) {
-    console.log(bigliettoScontatoO65);
-} 
-
-if (age >= 18 || age <= 64) {
-    console.log(biglietto);
-}
-
-document.getElementById("biglietto").innerHTML = biglietto
-document.getElementById("biglietto").innerHTML = bigliettoScontatoU18
-document.getElementById("biglietto").innerHTML = bigliettoScontatoO65
+const pricePerkm = 0.21;
+const discountChildren = 20;
+const discountSenior = 40;
